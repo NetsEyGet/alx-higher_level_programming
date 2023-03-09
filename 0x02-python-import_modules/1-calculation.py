c273be453b5d231db18ee85968433b240a9b27a3
Netsanet <eliyabeyob24@gmail.com>
2023-03-09 04:56:18 -0800

    Done

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,14 @@
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 
+
+def principal():
     a = 10
     b = 5
-    print('{:d} + {:d} = {:d}'.format(a, b, (add(a, b))))
-    print('{:d} - {:d} = {:d}'.format(a, b, (sub(a, b))))
-    print('{:d} * {:d} = {:d}'.format(a, b, (mul(a, b))))
-    print('{:d} / {:d} = {:d}'.format(a, b, (div(a, b))))
+    print('{} + {} = {}'.format(a, b, add(a, b)))
+    print('{} - {} = {}'.format(a, b, sub(a, b)))
+    print('{} * {} = {}'.format(a, b, mul(a, b)))
+    print('{} / {} = {}'.format(a, b, div(a, b)))
+
+if __name__ == "__main__":
+    principal()
