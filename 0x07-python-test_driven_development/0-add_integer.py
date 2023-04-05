a72120e9edb84e8ed1287afa7086f723837e07ff
Netsanet <eliyabeyob24@gmail.com>
2023-04-04 23:09:05 -0700

    Done

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
+"""
+This function is used to add two integers
+"""
+
+
 def add_integer(a, b=98):
-    if not isinstance(a, int) and not isinstance(a, float):
+    """
+    This mathod raises error if it is not integer or float
+    Args: a: The fist number to be added
+          b: The second number to be added
+    Return: returns the sum of both number
+    """
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
-    return (a + b)
+    return a + b
